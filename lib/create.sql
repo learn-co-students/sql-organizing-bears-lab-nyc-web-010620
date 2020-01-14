f015e22635601c8c2ff7b79c22ee788b8a76bd99
Miles Higbie <miles.higbie@gmail.com>
2020-01-14 16:46:23 -0500

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,13 @@
+CREATE TABLE bears (
+id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    gender TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive TEXT  
+); 
+
+
+
+-- INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
